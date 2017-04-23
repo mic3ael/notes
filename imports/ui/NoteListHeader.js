ff88fc52762ff91db064e7ce92990b3432b43da2
Michael Horojanski <mic3ael@gmail.com>
2017-04-24 00:36:16 +0300

    Select newly created note. Sort notes by updatedAt

--- imports/ui/NoteListHeader.js
+++ imports/ui/NoteListHeader.js
@@ -3,19 +3,25 @@ import {Meteor} from 'meteor/meteor';
 import {createContainer} from 'meteor/react-meteor-data';
 import {Notes} from '../api/notes';
 import PropTypes from 'prop-types';
+import {Session} from 'meteor/session';
 
 export const NoteListHeader = (props) => {
   return (
     <div>
       <button onClick={() => {
-        props.meteorCall('notes.insert');
+        props.meteorCall('notes.insert', (err, res) => {
+          if (res)
+            props.Session.set('selectedNoteId', res);
+          }
+        );
       }}>Create Note</button>
     </div>
   );
 };
 
 NoteListHeader.propTypes = {
-  meteorCall: PropTypes.func.isRequired
+  meteorCall: PropTypes.func.isRequired,
+  Session: PropTypes.object.isRequired
 };
 
 //middleware function
